e8cd8b293bb39b5b9048cd86be7042d20f006347
Doohaey Re <doohaey@gmail.com>
2025-08-14 14:30:31 +0800

    refactor: delete client module.

--- src/client/kotlin/com/imyvm/ImyvmAnnounceLiteClient.kt
+++
@@ -1,9 +0,0 @@
-package com.imyvm
-
-import net.fabricmc.api.ClientModInitializer
-
-object ImyvmAnnounceLiteClient : ClientModInitializer {
-	override fun onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-	}
-}
